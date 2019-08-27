3cf284b8f620102e3f1ee6e3d6d86e5af4338cac
Jessica Felts <thejessicafelts@gmail.com>
2019-08-27 11:17:13 -0400

    JS Reference Types Examples

---
+++ js_reference-types.js
@@ -0,0 +1,17 @@
+// Reference Types
+
+// Arrays
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+
+// Object Literals
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+console.log(typeof address);
+
+// Dates
+const today = new Date();
+console.log(today);
+console.log(typeof today);
