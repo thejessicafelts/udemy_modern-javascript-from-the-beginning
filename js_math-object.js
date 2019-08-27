553543c9c88a2f361da4efecb3669901be0ca639
Jessica Felts <thejessicafelts@gmail.com>
2019-08-27 16:33:57 -0400

    JS Math Object Examples

---
+++ js_math-object.js
@@ -0,0 +1,28 @@
+// Numbers and Math
+
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple Math with Numbers
+val = num1 + num2; // 100 + 50 = 150
+val = num1 * num2; // 100 * 50 = 5000
+val = num1 - num2; // 100 - 50 = 50
+val = num1 / num2; // 100 / 50 = 2
+val = num1 % num2; // 100 % 50 = 0
+
+// Math Object
+val = Math.PI; // Returns Pi - 3.141592653589793...
+val = Math.E; // Returns Euler's Number - 2.718281828459045...
+val = Math.round(2.8); // Rounds to 3
+val = Math.round(2.4); // Rounds to 2
+val = Math.ceil(2.4); // Rounds up to 3
+val = Math.floor(2.8); // Rounds down to 2
+val = Math.sqrt(64); // Returns the Square Root
+val = Math.abs(-15); // Returns the Absolute Number
+val = Math.pow(8, 2); // Returns the first number, to the power of the second number
+val = Math.min(2,3,5,9,15); // Returns the lowest value
+val = Math.max(2,3,5,9,15); // Returns the highest number
+val = Math.random(); // Returns a random decimal
+
+console.log(val);
