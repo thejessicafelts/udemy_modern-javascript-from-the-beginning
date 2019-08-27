7fd689cd6a1d041e6a5c1c3fb513695fccfd3829
Jessica Felts <thejessicafelts@gmail.com>
2019-08-27 10:47:31 -0400

    JS Console Logging Examples

---
+++ js_logging-to-the-console.js
@@ -0,0 +1,42 @@
+// Logging to the Console
+
+// String
+console.log('Hello World');
+
+// Number
+console.log(123);
+
+// Boolean
+console.log(true);
+console.log(false);
+
+// Variable
+var greeting = 'Hello';
+console.log(greeting);
+
+// Array
+console.log([1,2,3,4]);
+
+// Object
+console.log({a:1, b:2});
+
+// Object in Table
+console.table({a:1, b:2});
+
+// Error
+console.error('This is some error');
+
+// Warning
+console.warn('This is some warning');
+
+// Timer
+console.time('Hello');
+    console.log('Hello World'); 
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+console.timeEnd('Hello');
+
+// Clear the Console
+console.clear();
